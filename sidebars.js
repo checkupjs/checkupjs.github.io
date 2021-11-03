fbf04d818e54514ce5437345aab0c1e1a1e546c0
Steve Calvert <scalvert@linkedin.com>
2021-11-03 09:26:20 -0700

    Adding migration task documentation

--- sidebars.js
+++ sidebars.js
@@ -24,6 +24,7 @@ module.exports = {
           label: 'Authoring Tasks',
           items: [
             { type: 'doc', id: 'guides/writing-tasks' },
+            { type: 'doc', id: 'guides/writing-migration-tasks' },
             { type: 'doc', id: 'guides/actions' },
             { type: 'doc', id: 'guides/analyzers' },
             { type: 'doc', id: 'guides/generating-results' },
